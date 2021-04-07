4d527e5186dd75a402fd7f150804d01696257dc5
papasmf1 <papasmf1@gmail.com>
2021-04-07 13:15:24 +0900

    커밋 연습

---
+++ DemoDB2.py
@@ -0,0 +1,30 @@
+# DemoDB2.py 
+#SQLite를 사용하는 데모(로컬 데이터베이스)
+import sqlite3
+
+#처음에는 데이터베이스파일에 저장
+con = sqlite3.connect("c:\\work2\\sample.db")
+#SQL구문을 실행하는 것은 대부분 커서 객체
+cur = con.cursor()
+#저장소(테이블)을 만들기: 테이블 스키마(뼈대)
+cur.execute("create table PhoneBook (Name text, PhoneNum text);")
+#1건 입력
+cur.execute("insert into PhoneBook values ('derick', '010-111');")
+#입력 파라메터 처리(python format {0},{1})
+#텍스트박스(GUI, Web Page)에서 입력을 받아서 처리 
+name = "gildong"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook values (?, ?);", (name, phoneNumber))
+#다중의 레코드(행, row)를 입력받는 경우: 2차원 행열데이터 
+datalist = (("tom","010-123"), ("dsp","010-567"))
+cur.executemany("insert into PhoneBook values (?, ?);", datalist)
+
+#검색
+cur.execute("select * from PhoneBook;")
+for row in cur:
+    print(row)
+
+#커밋(작업을 정상적으로 완료. log --> db에도 기록) 
+#데이터를 변경(입력, 수정, 삭제)
+con.commit()
+
