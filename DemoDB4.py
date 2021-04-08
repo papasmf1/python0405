73e962d5fd2e6edf88b4936ad7040f52818606a5
papasmf1 <papasmf1@gmail.com>
2021-04-08 10:16:12 +0900

    DB예제

---
+++ DemoDB4.py
@@ -0,0 +1,12 @@
+#DemoDB4.py 
+import sqlite3
+
+#연결객체 리턴
+con = sqlite3.connect("c:\\work2\\Demo10.db")
+#커서객체 리턴
+cur = con.cursor()
+cur.execute("select * from Emp;")
+for row in cur:
+    print(row)
+
+    
